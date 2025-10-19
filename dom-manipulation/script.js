6bd14cd058dd065cf8dc9cf506b4bebc13f16ba3
GitMojoe <greatestmojoe.com>
2025-10-19 17:51:34 +0000

    adding importing json file

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -99,3 +99,13 @@ function exportQuotesToJSON() {
 }
 exportBtn.addEventListener('click', exportQuotesToJSON)   
  
+function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
